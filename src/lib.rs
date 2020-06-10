3b21e8d55d4fee701cb5a2c0d97fbedc1482e66e
Anton Whalley <anton@venshare.com>
2020-06-10 02:57:52 +0100

    retry

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub fn app() -> tide::Server<()> {    
     let mut api = tide::new();
-    api.at("/").get(|_| async move { Ok("Hello, Coligo From Actions!") });
+    api.at("/").get(|_| async move { Ok("Hello, Coligo & Actions!") });
     api
 }
